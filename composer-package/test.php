474b2afa127c318db678a7647533a872f65891cf
Leyuan <leyuan.ly@gmail.com>
2014-12-03 13:14:16 -0700

    Update test.php
    line 14

--- composer-package/test.php
+++ composer-package/test.php
@@ -7,9 +7,9 @@ use LoginRadiusSDK\LoginRadius;
 $api_secret = 'your_api_secret';
 
 if( LoginRadius::loginradius_is_callback() ) {
-	/* This will not be triggered since there is not _REQUEST['token']*/
+	/* This will not be triggered since there is no _REQUEST['token']*/
 	echo "call back succeed";
 } else {
 	/* Will run this*/
-	echo "Call back failed, but class is found.";
+	echo "Call back failed, but loginradius class is found.";
 }
