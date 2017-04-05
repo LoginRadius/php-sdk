6cd5857bcdf95fd0d3e6a9c827c744b2f0e45528
Karl-LoginRadius <feorn16@gmail.com>
2017-04-05 11:08:53 -0700

    Demo Update

--- demo/config.php
+++ demo/config.php
@@ -2,8 +2,8 @@
 if(session_id() == '') {
     session_start();
 }
-define('API_KEY','d41926c6-25cb-4f20-a7d1-a333403cb8d9');
-define('API_SECRET','781c3979-0e42-4a2e-bd80-2ff5f9c0d8fa');
+define('API_KEY','<APi Key>');
+define('API_SECRET','<API Secret>');
 define( 'DEMO_ROOT_DIR',dirname(__FILE__) );
 include 'LoginRadiusSDK/Utility/Functions.php';
 include 'LoginRadiusSDK/LoginRadiusException.php';
